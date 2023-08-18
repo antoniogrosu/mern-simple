4cbe9ffd1690de93f98883a401145c05d34005a5
Antonio Grosu <antonio.webdesign19@gmail.com>
2023-08-18 14:42:48 +0300

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,10 +3,6 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cors = require("cors");
-const path = require('path');
-
-const dataJsonPath = path.join(__dirname, '..', 'data.json');
-
 
 app.use(cors()); //cross origin for the frontend
 app.use(express.json());
@@ -35,7 +31,7 @@ app.post("/api/people", (req, res) => {
   if (!name) {
     return res.status(400).send("Provide name");
   }
-  fs.readFile(dataJsonPath, "utf-8", (readErr, data) => {
+  fs.readFile("data.json", "utf-8", (readErr, data) => {
     if (readErr) {
       console.error(readErr);
       return res.status(500).send(readErr);
@@ -44,7 +40,7 @@ app.post("/api/people", (req, res) => {
     let peopleData = JSON.parse(data); // arrray cu toate persoanele dupa parsare
     //aici doar se foloseste push pt ca altfel returneaza lungimea array-ului
     peopleData.push({ name: name });
-    fs.writeFile(dataJsonPath, JSON.stringify(peopleData), (writeErr) => {
+    fs.writeFile("data.json", JSON.stringify(peopleData), (writeErr) => {
       if (writeErr) {
         console.error(writeErr);
         return res.status(500).send(writeErr);
